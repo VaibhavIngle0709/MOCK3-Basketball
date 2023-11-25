a09c2752f5de0f67a70771c13d446e0bf6cd518e
Iamneo <techadmin@iamneo.ai>
2023-11-25 11:08:53 +0000

    Backup Commit

--- angularapp/src/app/services/positions.service.ts
+++ angularapp/src/app/services/positions.service.ts
@@ -6,8 +6,10 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class PositionsService {
+
   public apiURL="https://8080-ffbabdadc307818048dccbfcadeone.premiumproject.examly.io/api"
   constructor(private httpclient:HttpClient) { }
+  
   getPositions():Observable<any[]>
   {
     return this.httpclient.get<any[]>(this.apiURL+"/positions");
